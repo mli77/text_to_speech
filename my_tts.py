fd1bf2b68be2bf72cc618062f402ae5716e4a61b
Mli <mliotard@yahoo.fr>
2024-10-19 17:40:54 +0200

    test

---
+++ my_tts.py
@@ -0,0 +1,20 @@
+import torch
+from TTS.api import TTS
+
+# On doit acheter une licence très cher à un site qui est en shutdown et en plus le code ne fonctionne pas.
+
+# Get device
+device = "cuda" if torch.cuda.is_available() else "cpu"
+
+# List available 🐸TTS models
+print(TTS().list_models())
+
+# Init TTS
+tts = TTS("tts_models/multilingual/multi-dataset/xtts_v2").to(device)
+
+# Run TTS
+# ❗ Since this model is multi-lingual voice cloning model, we must set the target speaker_wav and language
+# Text to speech list of amplitude values as output
+wav = tts.tts(text="Hello world!", speaker_wav="my/cloning/audio.wav", language="en")
+# Text to speech to a file
+tts.tts_to_file(text="Hello world!", speaker_wav="my/cloning/audio.wav", language="en", file_path="output.wav")
